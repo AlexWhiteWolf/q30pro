9cdd94bcd12231623886df6d5282dd7ecc463a41
xqrock <58764635+xqrock@users.noreply.github.com>
2025-01-24 14:48:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,8 +29,8 @@ function drop_package(){
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/linkease/istore-ui.git package/istore-ui
+#git clone https://github.com/linkease/istore.git package/istore
+#git clone https://github.com/linkease/istore-ui.git package/istore-ui
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
